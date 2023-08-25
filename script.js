9071a9c4f77ce582ce18a29f8bc7de3b8795a0f7
Abdullah Alioğlu <abdullah.alioglu@bahcesehir.edu.tr>
2023-08-25 11:48:35 +0300

    update2

---
+++ script.js
@@ -0,0 +1,55 @@
+const myCarousel = document.getElementById("carouselExampleIndicators");
+const carouselIndicators = myCarousel.querySelectorAll(
+  ".carousel-indicators button span"
+);
+let intervalID;
+
+const carousel = new bootstrap.Carousel(myCarousel);
+
+window.addEventListener("load", function () {
+  fillCarouselIndicator(1);
+});
+
+myCarousel.addEventListener("slide.bs.carousel", function (e) {
+  let index = e.to;
+  fillCarouselIndicator(++index);
+});
+
+function fillCarouselIndicator(index) {
+  let i = 0;
+  for (const carouselIndicator of carouselIndicators) {
+    carouselIndicator.style.width = 0;
+  }
+  clearInterval(intervalID);
+  carousel.pause();
+}
+
+var $item = $(".carousel-item");
+var $wHeight = $(window).height();
+$item.eq(0).addClass("active");
+$item.height($wHeight);
+$item.addClass("full-screen");
+
+$(".carousel img").each(function () {
+  var $src = $(this).attr("src");
+  var $color = $(this).attr("data-color");
+  $(this)
+    .parent()
+    .css({
+      "background-image": "url(" + $src + ")",
+      "background-color": $color,
+    });
+  $(this).remove();
+});
+
+$(window).on("resize", function () {
+  $wHeight = $(window).height();
+  $item.height($wHeight);
+});
+
+$(".carousel").carousel({
+  interval: 6000,
+  pause: "false",
+});
+
+// navbar
