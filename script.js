1738a40745fa1f3daaa237e82f35f84494cb9dec
Abdullah Alioğlu <abdullah.alioglu@bahcesehir.edu.tr>
2023-09-05 09:39:34 +0300

    etaps-link added.

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-//document.addEventListener(function () {
+// scrolllink
 var scrolllink = document.getElementById("scrolllink");
 
 window.addEventListener("scroll", function () {
-  if (window.scrollY >= 500) {
+  if (window.scrollY >= 400) {
     scrolllink.style.display = "block";
   } else {
     scrolllink.style.display = "none";
   }
 });
-// });
+
+// ---------------------------------------------------------------------------------------------------
 
 const myCarousel = document.getElementById("carouselExampleIndicators");
 const carouselIndicators = myCarousel.querySelectorAll(
@@ -64,4 +65,62 @@ $(".carousel").carousel({
   pause: "false",
 });
 
-// scroll
+// Tıklanan link öğesini bulun
+var link = document.getElementById("link");
+
+// Gizli div'i bulun
+var gizliDiv = document.getElementById("gizliDiv");
+
+// Kapatma ikonunu bulun
+var kapatIcon = document.getElementById("kapatIcon");
+
+// Tıklama olayını dinleyin
+link.addEventListener("click", function (e) {
+  e.preventDefault(); // Sayfa yenilenmesini engellemek için gereken bir önlem
+  // Gizli div'i görünür hale getirin
+  gizliDiv.style.display = "block";
+});
+
+// Kapatma ikonuna tıklama olayını dinleyin
+kapatIcon.addEventListener("click", function () {
+  // Gizli div'i tekrar gizleyin
+  gizliDiv.style.display = "none";
+});
+
+// ***************************
+
+var link = document.getElementById("link");
+
+// Gizli div'i bulun
+var gizliDiv = document.getElementById("gizliDiv");
+
+// Tıklama olayını dinleyin
+link.addEventListener("click", function (e) {
+  e.preventDefault(); // Sayfa yenilenmesini engellemek için gereken bir önlem
+  // Gizli div'i görünür hale getirin
+  gizliDiv.style.display = "block";
+});
+
+var link5 = document.getElementById("link-5");
+
+// Gizli div'i bulun
+var gizliDiv5 = document.getElementById("gizliDiv-5");
+
+// Tıklama olayını dinleyin
+link5.addEventListener("click", function (e) {
+  e.preventDefault(); // Sayfa yenilenmesini engellemek için gereken bir önlem
+  // Gizli div'i görünür hale getirin
+  gizliDiv5.style.display = "block";
+});
+
+var link14 = document.getElementById("link-14");
+
+// Gizli div'i bulun
+var gizliDiv14 = document.getElementById("gizliDiv-14");
+
+// Tıklama olayını dinleyin
+link14.addEventListener("click", function (e) {
+  e.preventDefault(); // Sayfa yenilenmesini engellemek için gereken bir önlem
+  // Gizli div'i görünür hale getirin
+  gizliDiv14.style.display = "block";
+});
